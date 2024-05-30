aa3b28b2851eb17cc358932712a93ff070572403
Joao Bueno <joaobueno121212@gmail.com>
2024-05-30 16:13:40 -0300

    making the seed and first migration

---
+++ EcommerceAPI/Data/Seed.cs
@@ -0,0 +1,94 @@
+﻿using EcommerceAPI.Data.Enums;
+using EcommerceAPI.Models;
+
+namespace EcommerceAPI.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _dataContext;
+
+        public Seed(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void SeedData()
+        {
+            User user1 = new()
+            {
+                Name = "Admin",
+                Email = "AdminB@what.com",
+                Password = "Admin123",
+            };
+
+            if (!_dataContext.Users.Any())
+            {
+                User user2 = new()
+                {
+                    Name = "Admins",
+                    Email = "AdminBs@what.com",
+                    Password = "Admin123",
+                };
+
+                _dataContext.Users.AddRange(user1, user2);
+                _dataContext.SaveChanges();
+            }
+
+            if(!_dataContext.Products.Any())
+            {
+                Product product1 = new()
+                {
+                    Name = "Smartphone",
+                    Description = "Latest model smartphone with advanced features.",
+                    Price = 799.99,
+                    Stock = 50,
+                    Category = ProductCategory.Electronics,
+                    Owner = user1
+                };
+
+                Product product2 = new()
+                {
+                    Name = "Jeans",
+                    Description = "Comfortable and stylish jeans.",
+                    Price = 49.99,
+                    Stock = 50,
+                    Category = ProductCategory.Clothing,
+                    Owner = user1
+                };
+
+                Product product3 = new()
+                {
+                    Name = "Science Fiction Novel",
+                    Description = "A thrilling science fiction novel.",
+                    Price = 19.99,
+                    Stock = 50,
+                    Category = ProductCategory.Books,
+                    Owner = user1,
+                };
+
+                Product product4 = new()
+                {
+                    Name = "Gaming Laptop",
+                    Description = "High performance gaming laptop.",
+                    Price = 1299.99,
+                    Stock = 30,
+                    Category = ProductCategory.ComputersAndAccessories,
+                    Owner = user1,
+                };
+
+                Product product5 = new()
+                {
+                    Name = "Drone",
+                    Description = "Remote controlled drone with camera.",
+                    Price = 499.99,
+                    Stock = 35,
+                    Category = ProductCategory.Hobbies,
+                    Owner = user1,
+                };
+
+                _dataContext.Products.AddRange(product1, product2, product3, product4, product5);
+                _dataContext.SaveChanges();
+            }
+        }
+    }
+}
--- EcommerceAPI/Program.cs
+++ EcommerceAPI/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//add the seeding
+builder.Services.AddTransient<Seed>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +20,23 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
 var app = builder.Build();
+
+//seeding
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+    SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        service.SeedData();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
