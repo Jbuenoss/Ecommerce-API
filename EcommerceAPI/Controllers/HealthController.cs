5355ff21214b397b8d4a73a96ec66ae64f35710d
Joao Bueno <joaobueno121212@gmail.com>
2024-07-03 11:36:14 -0300

    Creating a new controller for health

---
+++ EcommerceAPI/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult GetHealth()
+        {
+            var healthStatus = new
+            {
+                status = "Healthy",
+                message = "API is running"
+            };
+
+            return Ok(healthStatus);
+        }
+    }
+}
